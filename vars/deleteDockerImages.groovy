0c671da3daee9cebd90fe559c48f5fe5a0c800a7
Muhammad Hewedy <mhewedy@gmail.com>
2020-12-11 12:39:14 +0300

    remove "sed '$d'" which means remove last line, as it stop the step from removing the docker image

--- vars/deleteDockerImages.groovy
+++ vars/deleteDockerImages.groovy
@@ -4,7 +4,7 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig) {
     def reference = projectConfig.image;
     try {
-        sh "docker images --filter 'reference=${reference}*' --format \"{{.Tag}} {{.Repository}}:{{.Tag}}\" | sort -n | sed '\$d' | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi"
+        sh "docker images --filter 'reference=${reference}*' --format \"{{.Tag}} {{.Repository}}:{{.Tag}}\" | sort -n | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi"
 
     } catch(ignored) {
         // this would make the entire popeline fail. We don't want that
